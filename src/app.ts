240298de2bf721f3a70014574dc999a0848db3df
KHTangent <contact@khtangent.com>
2022-07-22 20:00:50 +0200

    Improve config handling

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,33 @@
 import * as Discord from "discord.js";
 import "dotenv/config";
-import { checkDotEnv, isLeetMessage } from "./utils";
+import { loadConfig, isLeetMessage } from "./utils";
 
-checkDotEnv(); // Halt if a required parameter is missing
-const TIMEZONE = parseInt(process.env.TIMEZONE!);
+const config = loadConfig(); // Halts if a required parameter is missing
 
 (async () => {
-  console.log("Starting leetbot...");
-  const bot = new Discord.Client({
-    intents: [
-      Discord.GatewayIntentBits.Guilds,
-      Discord.GatewayIntentBits.GuildMessages,
-      Discord.GatewayIntentBits.DirectMessages,
-      Discord.GatewayIntentBits.MessageContent,
-    ],
-  });
+	console.log("Starting leetbot...");
+	const bot = new Discord.Client({
+		intents: [
+			Discord.GatewayIntentBits.Guilds,
+			Discord.GatewayIntentBits.GuildMessages,
+			Discord.GatewayIntentBits.DirectMessages,
+			Discord.GatewayIntentBits.MessageContent,
+		],
+	});
 
-  bot.on("ready", () => {
-    console.log(`Logged in as ${bot.user?.username}`);
-  });
+	bot.on("ready", () => {
+		console.log(`Logged in as ${bot.user?.username}`);
+	});
 
-  bot.on("messageCreate", (msg) => {
-    if (msg.content === "leet") {
-      if (isLeetMessage(BigInt(msg.id), TIMEZONE)) {
-        msg.channel.send("Good leet");
-      } else {
-        msg.channel.send("Bad leet");
-      }
-    }
-  });
+	bot.on("messageCreate", (msg) => {
+		if (msg.content === "leet") {
+			if (isLeetMessage(BigInt(msg.id), config.timezone)) {
+				msg.channel.send("Good leet");
+			} else {
+				msg.channel.send("Bad leet");
+			}
+		}
+	});
 
-  bot.login(process.env.DISCORD_TOKEN);
+	bot.login(config.token);
 })();
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,31 @@
 export function isLeetMessage(messageId: bigint, timezone = 0): boolean {
-  const t = new Date(Number((messageId >> 22n) + 1420070400000n));
-  return t.getUTCHours() + timezone === 13 && t.getUTCMinutes() === 37;
+	const t = new Date(Number((messageId >> 22n) + 1420070400000n));
+	return t.getUTCHours() + timezone === 13 && t.getUTCMinutes() === 37;
 }
 
-export function checkDotEnv() {
-  const required = ["DISCORD_TOKEN", "TIMEZONE"];
-  for (const param of required) {
-    if (!process.env[param]) {
-      console.log("Missing required environment variable: " + param);
-      process.exit(1);
-    }
-  }
+export interface Configuration {
+	token: string;
+	timezone: number;
+}
+
+export function loadConfig(): Configuration {
+	checkDotEnv();
+	const timezone = parseInt(process.env["TIMEZONE"]!);
+	if (isNaN(timezone)) {
+		console.error("TIMEZONE must be an integer");
+	}
+	return {
+		timezone,
+		token: process.env["DISCORD_TOKEN"]!,
+	};
+}
+
+function checkDotEnv() {
+	const required = ["DISCORD_TOKEN", "TIMEZONE"];
+	for (const param of required) {
+		if (!process.env[param]) {
+			console.error("Missing required environment variable: " + param);
+			process.exit(1);
+		}
+	}
 }
